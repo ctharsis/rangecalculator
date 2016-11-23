c69bea9e90a72169f3bbcc08e13b48ba94cb3351
Ernie <ernieyeung123@gmail.com>
2016-11-23 15:45:07 -0800

    added lots of stuff

--- data/masterydata.js
+++ data/masterydata.js
@@ -1,2 +1,5 @@
 var masteryData = new Object();
+masteryData["hero1ha"] = 0.9; 
+masteryData["hero2ha"] = 0.9; 
 masteryData["hero1hs"] = 0.9; 
+masteryData["hero2hs"] = 0.9; 
--- data/values.js
+++ data/values.js
@@ -42,9 +42,18 @@ classWep["shade"] = "knuckle";
 
 //access other stats by linkSkills[""].att etc.
 var linkSkills = new Object();
+linkSkills["demonAvenger0"] = {att:0, pDmg:0};
 linkSkills["demonAvenger1"] = {att:0, pDmg:5};
 linkSkills["demonAvenger2"] = {att:0, pDmg:10};	
 linkSkills["demonAvenger3"] = {att:0, pDmg:15};
+linkSkills["kanna0"] = {att:0, pDmg:0};
 linkSkills["kanna1"] = {att:0, pDmg:5};
 linkSkills["kanna2"] = {att:0, pDmg:10};
 linkSkills["hayato"] = {att:5, pDmg:0};
+
+//access other stats by passiveStat[""].att etc.
+var passiveStat = new Object();
+passiveStat["hero1ha"] = {att:30, pDmg:5};
+passiveStat["hero2ha"] = {att:30, pDmg:5};
+passiveStat["hero1hs"] = {att:30, pDmg:0};
+passiveStat["hero2hs"] = {att:30, pDmg:0};
