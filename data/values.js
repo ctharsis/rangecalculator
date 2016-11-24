79c4e54517b9fb7022adb35beb6e262233d12572
Ernie <ernieyeung123@gmail.com>
2016-11-23 18:32:50 -0800

    added lots of classes

--- data/values.js
+++ data/values.js
@@ -1,6 +1,6 @@
 // weapon multiplier values
 var wepMult = new Object();
-wepMult["oneHandSword"] = 1.30; //Maplewikia is wrong
+wepMult["oneHandSword"] = 1.30; //Maplewikia is wrong, at least for hero.
 wepMult["twoHandSword"] = 1.34;
 wepMult["oneHandAxe"] = 1.20;
 wepMult["twoHandAxe"] = 1.34;
@@ -34,9 +34,22 @@ wepMult["psylimiter"] = 1.20;
 
 // weapons that the classes use
 var classWep = new Object();
+classWep["bishopstaff"] = "staffExplorer"
+classWep["bishopwand"] = "wandExplorer"
+classWep["evanstaff"] = "staff";
+classWep["evanwand"] = "wand";
+classWep["firepoisonstaff"] = "staffExplorer"
+classWep["firepoisonwand"] = "wandExplorer"
+classWep["hero1ha"] = "oneHandAxe";
+classWep["hero2ha"] ="twoHandAxe"; 
 classWep["hero1hs"] = "oneHandSword";
-classWep["hero2hs"] ="twoHandSword"; // test
+classWep["hero2hs"] ="twoHandSword";
+classWep["icelightningstaff"] = "staffExplorer"
+classWep["icelightningwand"] = "wandExplorer"
+classWep["kanna"] = "fan";
 classWep["kinesis"] = "psylimiter";
+classWep["luminous"] = "shiningRod";
+classWep["nightlord"] = "claw";
 classWep["phantom"] = "cane";
 classWep["shade"] = "knuckle";
 
@@ -53,7 +66,23 @@ linkSkills["hayato"] = {att:5, pDmg:0};
 
 //access other stats by passiveStat[""].att etc.
 var passiveStat = new Object();
-passiveStat["hero1ha"] = {att:30, pDmg:5};
-passiveStat["hero2ha"] = {att:30, pDmg:5};
-passiveStat["hero1hs"] = {att:30, pDmg:0};
-passiveStat["hero2hs"] = {att:30, pDmg:0};
+passiveStat["bishopstaff"] = {att:40, pAtt:0, pDmg:0};
+passiveStat["bishopwand"] = {att:40, pAtt:0, pDmg:0};
+passiveStat["evanstaff"] = {att:70, pAtt:35, pDmg:0};
+passiveStat["evanwand"] = {att:70, pAtt:35, pDmg:0};
+passiveStat["firepoisonstaff"] = {att:40, pAtt:50, pDmg:0};
+passiveStat["firepoisonwand"] = {att:40, pAtt:50, pDmg:0};
+passiveStat["hero1ha"] = {att:30, pAtt:0, pDmg:5};
+passiveStat["hero2ha"] = {att:30, pAtt:0, pDmg:5};
+passiveStat["hero1hs"] = {att:30, pAtt:0, pDmg:0};
+passiveStat["hero2hs"] = {att:30, pAtt:0, pDmg:0};
+passiveStat["icelightningstaff"] = {att:40, pAtt:50, pDmg:0};
+passiveStat["icelightningwand"] = {att:40, pAtt:50, pDmg:0};
+passiveStat["kanna"] = {att:30, pAtt:0, pDmg:30};
+passiveStat["kinesis"] = {att:50, pAtt:0, pDmg:0};
+passiveStat["luminous"] = {att:40, pAtt:0, pDmg:0};
+passiveStat["nightlord"] = {att:70, pAtt:0, pDmg:0};
+passiveStat["phantom"] = {att:40, pAtt:0, pDmg:0};
+passiveStat["shade"] = {att:20, pAtt:0, pDmg:20};
+
+//passiveStat[""] = {att:0, pAtt:0, pDmg:0};
